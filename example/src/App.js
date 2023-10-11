31d8fca6df9620501d035ff9407715d94861de68
akshayshelkeengg <akshayshelkeengg@gmail.com>
2023-10-11 15:11:21 +0530

    tab customization added

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import TabComponent from 'rc-easy-tabs'
-import 'rc-easy-tabs/dist/index.css'
+//import 'rc-easy-tabs/dist/component/TabComponent.css'
 import Tab1 from './Tab1'
 import Tab2 from './Tab2'
 
@@ -10,23 +10,21 @@ const App = () => {
     background:'#C0CFFA',
     borderStyle: 'solid',
     borderWidth: '1px',
-    padding:'4px',
-    borderTopLeftRadius: '10px',
-    borderTopRightRadius: '10px',
+    padding:'4px', 
     cursor: 'pointer',
   }
 
   const selectedTab= {
     borderStyle: 'solid',
     borderWidth: '1px',
-    padding:'6px',
-    borderTopLeftRadius: '10px',
-    borderTopRightRadius: '10px',
+    padding:'6px',    
     cursor: 'pointer',
     background:'#48578E',    
     color:'white'
   }  
   const [tabs,setTabs]=useState([
+    {title:"Tab1",component:<Tab1/>},
+    {title:"Tab2",component:<Tab2/>},   
     {title:"Tab1",component:<Tab1/>},
     {title:"Tab2",component:<Tab2/>},   
   ])
---
+++ src/component/TabComponent.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const style = {
+  singleTab:{
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    padding:'4px',
+    borderTopLeftRadius: '10px',
+    borderTopRightRadius: '10px',
+    cursor:'pointer'
+},
+
+  selectedTab: {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    padding:'6px',
+    borderTopLeftRadius: '10px',
+    borderTopRightRadius: '10px',
+    cursor: 'pointer',
+    background:'#ccc',
+  },  
+};
+
+export default function TabComponent({tabsData,tabStyle,selectedTabStyle}) {  
+
+  const [tabs,setTabs]=useState(tabsData)
+  const [propTabStyle,setPropTabStyle]=useState()
+  const [propSelectedTabStyle,setPropSelectedTabStyle]=useState()
+
+
+  useEffect(()=>{
+    if(tabStyle!==undefined){
+      var newPropStyle={}
+      if('borderTopLeftRadius' in tabStyle){
+        console.log("Exists")
+        newPropStyle={...tabStyle,borderTopLeftRadius:'10px',borderTopRightRadius: '10px',}
+      }
+      else{
+        console.log("not Exists")
+        newPropStyle={...tabStyle,borderTopLeftRadius:'10px',borderTopRightRadius: '10px',}
+      }
+      setPropTabStyle(newPropStyle)
+    }
+    if(selectedTabStyle!==undefined){
+      if('borderTopLeftRadius' in selectedTabStyle){
+        var newSelectedPropStyle={}
+        console.log("Exists")
+        newSelectedPropStyle={...selectedTabStyle,borderTopLeftRadius:'10px',borderTopRightRadius: '10px',}
+      }
+      else{
+        console.log("not Exists")
+        newSelectedPropStyle={...selectedTabStyle,borderTopLeftRadius:'10px',borderTopRightRadius: '10px',}
+      }
+      setPropSelectedTabStyle(newSelectedPropStyle)
+    }
+    
+  },[tabStyle,selectedTabStyle])
+
+  useEffect(()=>{
+    const newTabsData=tabs.map((tab,index)=>{
+      if(index==0){
+        return {...tab,selected:true}
+      }
+      else{
+        return {...tab,selected:false}
+      }
+    })
+    setTabs(newTabsData)
+  },[])
+
+  const handleTabSelection=(selectedIndex)=>{
+    const newTabsData=tabs.map((tab,index)=>{
+      if(selectedIndex==index){
+        return {...tab,selected:true}
+      }
+      else{
+        return{...tab,selected:false}
+      }
+    })
+    setTabs(newTabsData)
+  }
+
+  return (    
+    <div style={{margin:'10px'}}>
+      <div className='row'>
+        TabComponent
+      </div>
+      <div className='row' >        
+        {(tabStyle===undefined && selectedTabStyle===undefined) && tabs.map((tab,index)=>{
+         
+          return (          
+          <div key={index} className={`col-lg-3 col-md-4 col-sm-9`} style={tab.selected?style.selectedTab:style.singleTab} onClick={()=>handleTabSelection(index)}>
+              {tab.title}
+          </div>)
+        })}
+        {(tabStyle!==undefined && selectedTabStyle!==undefined) && tabs.map((tab,index)=>{         
+          return (          
+          <div key={index} className={`col-lg-3 col-md-4 col-sm-9`} style={tab.selected?propSelectedTabStyle:propTabStyle} onClick={()=>handleTabSelection(index)}>
+              {tab.title}
+          </div>)
+        })}
+         {(tabStyle===undefined && selectedTabStyle!==undefined) && tabs.map((tab,index)=>{         
+          return (          
+          <div key={index} className={`col-lg-3 col-md-4 col-sm-9`} style={tab.selected?propSelectedTabStyle:style.singleTab} onClick={()=>handleTabSelection(index)}>
+              {tab.title}
+          </div>)
+        })}
+          {(tabStyle!==undefined && selectedTabStyle===undefined) && tabs.map((tab,index)=>{         
+          return (          
+          <div key={index} className={`col-lg-3 col-md-4 col-sm-9`} style={tab.selected?style.selectedTab:propTabStyle} onClick={()=>handleTabSelection(index)}>
+              {tab.title}
+          </div>)
+        })}
+      
+        <div className='row'> 
+          {tabs.map((tab,index)=>{
+            return (
+            <div key={index} className='col-lg-12 col-md-12 col-sm-12'>
+                {tab.selected && tab.component}
+            </div>)
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
